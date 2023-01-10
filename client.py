a44cbdbd65eb606ab40c781c7f469be09a433d55
mitkobochev <mitkobochev@icloud.com>
2023-01-10 20:50:03 +0200

    Uploading requirements.txt file and separating code to functions

--- client.py
+++ client.py
@@ -6,60 +6,92 @@ import dateutil.parser
 from datetime import datetime
 from auth import updated_token
 
-# Parse input arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('-k', '--keys', nargs='+', required=True)
-parser.add_argument('-c', '--colored', action='store_true', default=True)
-args = parser.parse_args()
+def parse_args():
+    """
+    Taking the input parameters 
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-k', '--keys', nargs='+', required=True)
+    parser.add_argument('-c', '--colored', action='store_true', default=True)
+    return parser.parse_args()
 
-# Read CSV file
-with open('vehicles.csv', 'r') as f:
-    reader = csv.DictReader(f)
-    vehicles = list(reader)
+def read_csv(file):
+    """
+    Read csv file and returns list of dictionaries
+    """
+    with open(file, 'r') as f:
+        reader = csv.DictReader(f)
+        return list(reader)
 
-# Transmit CSV to server and retrieve response
-url = 'http://localhost:8000/vehicles'
-headers = {'Authorization': f'Bearer {updated_token}'}
-response = requests.post(url, json=vehicles)
-vehicles = response.json()
+def transmit_csv(vehicles, headers):
+    """
+    Transmit the vehicles to API
+    """
+    url = 'http://localhost:8000/vehicles'
+    response = requests.post(url, json=vehicles, headers=headers)
+    return response.json()
 
-# Sort vehicles by gruppe
-vehicles = sorted(vehicles, key=lambda x: x['gruppe'])
-for vehicle in vehicles:
-    for key in args.keys:
-        if key in vehicle:
-            vehicle[key] = vehicle[key]
+def sort_vehicles(vehicles):
+    """
+    Sort the vehicles by gruppe field
+    """
+    return sorted(vehicles, key=lambda x: x['gruppe'])
 
-# Save to Excel file
-current_date = datetime.now().date().isoformat()
-filename = f'vehicles_{current_date}.xlsx'
-workbook = xlsxwriter.Workbook(filename)
-worksheet = workbook.add_worksheet()
+def color_cells(workbook,worksheet, vehicles, keys):
+    """
+    Add conditional formatting to excel cells
+    """
+    red_format = workbook.add_format({'color': '#b30000'})
+    orange_format = workbook.add_format({'color': '#FFA500'})
+    green_format = workbook.add_format({'color': '#007500'})
 
-# Add header
-headers = list(vehicles[0].keys())
-worksheet.write_row(0, 0, headers)
+    for i, row in enumerate(vehicles, start=1):
+        for j, key in enumerate(row):
+            if keys and key == 'hu' and row[key] is not None:
+                    hu_date = dateutil.parser.parse(row[key])
+                    hu_age_in_months = (datetime.now() - hu_date).days / 30
+                    if hu_age_in_months <= 3:
+                        worksheet.write(i, j, row[key], green_format)
+                    elif hu_age_in_months <= 12:
+                        worksheet.write(i, j, row[key], orange_format)
+                    else:
+                        worksheet.write(i, j, row[key], red_format)
+            else:
+                worksheet.write(i, j, row[key])
 
-# Color cells based on ColorCode
-red_format = workbook.add_format({'color': '#b30000'})
-orange_format = workbook.add_format({'color': '#FFA500'})
-green_format = workbook.add_format({'color': '#007500'})
+def add_header(worksheet, vehicles):
+    """
+    Add headers to worksheet
+    """
+    headers = list(vehicles[0].keys())
+    worksheet.write_row(0, 0, headers)
 
-for i, row in enumerate(vehicles, start=1):
-    for j, key in enumerate(row):
-        if args.colored and key == 'hu' and row[key] is not None:
-                # Set cell's text color based on HU
-                hu_date = dateutil.parser.parse(row[key])
-                hu_age_in_months = (datetime.now() - hu_date).days / 30
-                if hu_age_in_months <= 3:
-                    worksheet.write(i, j, row[key], green_format)
-                elif hu_age_in_months <= 12:
-                    worksheet.write(i, j, row[key], orange_format)
-                else:
-                    worksheet.write(i, j, row[key], red_format)
-        else:
-            worksheet.write(i, j, row[key])
+def save_to_excel(vehicles, colored, keys):
+    """
+    Save vehicles to excel
+    """
+    current_date = datetime.now().date().isoformat()
+    filename = f'vehicles_{current_date}.xlsx'
+    workbook = xlsxwriter.Workbook(filename)
+    worksheet = workbook.add_worksheet()
 
+    add_header(worksheet, vehicles)
+    color_cells(workbook,worksheet, vehicles, colored)
 
-workbook.close()
-print(f'Saved as {filename}')
+    workbook.close()
+    print(f'Saved as {filename}')
+
+def main():
+    """
+    This function call all above functions in order 
+    """
+    args = parse_args()
+    data = read_csv('vehicles.csv')
+    headers = {'Authorization': f'Bearer {updated_token}'}
+    data = transmit_csv(data, headers)
+    data = sort_vehicles(data)
+    current_date = datetime.now().date().isoformat()
+    filename = f'vehicles_{current_date}.xlsx'
+    save_to_excel(data, filename, args.colored)
+
+main()
--- server.py
+++ server.py
@@ -4,30 +4,52 @@ from auth import updated_token
 
 app = FastAPI()
 
-@app.post("/vehicles")
-async def merge_vehicles(request: Request) -> list:
-    # Retrieve request body as CSV
+async def parse_csv_body(request: Request) -> list:
+    """
+    Takes a Request object and return list of vehicle in format of dictionary
+    """
     csv = await request.body()
     csv = csv.decode("utf-8")
     rows = csv.split("\n")
     keys = rows[0].split(",")
     vehicles = [dict(zip(keys, row.split(","))) for row in rows[1:] if row]
-    
-    # Request additional resources and merge with request body
+    return vehicles
+
+def fetch_additional_resources(vehicles: list) -> list:
+    """
+    Fetch additional resources from external API and merge it with the vehicles list
+    """
     api_url = "https://api.baubuddy.de/dev/index.php/v1/vehicles/select/active"
     headers = {"Authorization": f"Bearer {updated_token}"}
     api_response = requests.get(api_url, headers=headers).json()
     vehicles += api_response
-    
-    # Filter by HU field
-    vehicles = [vehicle for vehicle in vehicles if "hu" in vehicle]
+    return vehicles
 
-    # Resolve labelIds and add colorCode field
+def filter_by_hu(vehicles: list) -> list:
+    """
+    Filters the vehicles based on the "hu" field
+    """
+    vehicles = [vehicle for vehicle in vehicles if "hu" in vehicle]
+    return vehicles
+
+def resolve_label_ids(vehicles: list) -> list:
+    """
+    Resolves the labelIds and add colorCode field to the vehicles
+    """
     if any("labelId" in vehicle for vehicle in vehicles):
         label_url = "https://api.baubuddy.de/dev/index.php/v1/labels/{vehicle}"
         for vehicle in vehicles:
             if "labelIds" in vehicle:
                 vehicle["colorCode"] = requests.get(label_url.format(vehicle["labelIds"])).json()
-                return vehicle
+    return vehicles
 
+@app.post("/vehicles")
+async def merge_vehicles(request: Request) -> list:
+    """
+    This function call all above functions in order 
+    """
+    vehicles = await parse_csv_body(request)
+    vehicles = fetch_additional_resources(vehicles)
+    vehicles = filter_by_hu(vehicles)
+    vehicles = resolve_label_ids(vehicles)
     return vehicles
